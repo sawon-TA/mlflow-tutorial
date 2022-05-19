f4aba4a70cbc19a865cf8731f1bcea4be737711c
sawon-TA <sawon.bhattachar@tigeranalytics.com>
2022-05-19 15:16:23 +0530

    ml-flow tutorial added

---
+++ README.md
@@ -0,0 +1,21 @@
+## ML-FLOW WALKTHROUGH
+1. Install the mlflow package
+```
+pip install mlflow
+```
+2. Structure the code by writing proper functions for different process
+3. keep the <b>mlflow.start_run()</b> function and save the file as a '.py' script
+4. Run the file
+
+```
+python mlflow_test.py
+```
+
+5. Start the mlflow UI
+```
+mlflow ui
+```
+6. Run the MLflow server 
+```
+mlflow server --backend-store-uri mlruns/ --default-artifact-root mlruns/ --host 0.0.0.0 --port 5000
+```
---
+++ mlflow_test.py
@@ -0,0 +1,86 @@
+# The data set used in this example is from http://archive.ics.uci.edu/ml/datasets/Wine+Quality
+# P. Cortez, A. Cerdeira, F. Almeida, T. Matos and J. Reis.
+# Modeling wine preferences by data mining from physicochemical properties. In Decision Support Systems, Elsevier, 47(4):547-553, 2009.
+
+import logging
+import os
+import sys
+import warnings
+from urllib.parse import urlparse
+
+import mlflow
+import mlflow.sklearn
+import numpy as np
+import pandas as pd
+from sklearn.linear_model import ElasticNet
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+from sklearn.model_selection import train_test_split
+
+logging.basicConfig(level=logging.WARN)
+logger = logging.getLogger(__name__)
+
+DATA_PATH = "ml_data/winequality-red.csv"
+
+
+def eval_metrics(actual, pred):
+    rmse = np.sqrt(mean_squared_error(actual, pred))
+    mae = mean_absolute_error(actual, pred)
+    r2 = r2_score(actual, pred)
+    return rmse, mae, r2
+
+
+def data_load(csv_url=DATA_PATH):
+    # Read the wine-quality csv file from the URL
+    try:
+        data = pd.read_csv(csv_url, sep=";")
+    except Exception as e:
+        logger.exception(
+            "Unable to download training & test CSV, check your internet connection. Error: %s",
+            e,
+        )
+
+    # Split the data into training and test sets. (0.75, 0.25) split.
+    train, test = train_test_split(data)
+
+    # The predicted column is "quality" which is a scalar from [3, 9]
+    train_x = train.drop(["quality"], axis=1)
+    test_x = test.drop(["quality"], axis=1)
+    train_y = train[["quality"]]
+    test_y = test[["quality"]]
+    return train_x, train_y, test_x, test_y
+
+
+def train(alpha=0.5, l1_ratio=0.5):
+    warnings.filterwarnings("ignore")
+    np.random.seed(40)
+
+    # data loading
+    train_x, train_y, test_x, test_y = data_load(DATA_PATH)
+    with mlflow.start_run():
+        # Execute ElasticNet
+        lr = ElasticNet(alpha=alpha, l1_ratio=l1_ratio, random_state=42)
+        lr.fit(train_x, train_y)
+
+        predicted_qualities = lr.predict(test_x)
+        # Evaluate Metrics
+        (rmse, mae, r2) = eval_metrics(test_y, predicted_qualities)
+        # Print out metrics
+        print("Elasticnet model (alpha=%f, l1_ratio=%f):" % (alpha, l1_ratio))
+        print("  RMSE: %s" % rmse)
+        print("  MAE: %s" % mae)
+        print("  R2: %s" % r2)
+
+        # Log parameter, metrics, and model to MLflow
+        mlflow.log_param("alpha", alpha)
+        mlflow.log_param("l1_ratio", l1_ratio)
+        mlflow.log_metric("rmse", rmse)
+        mlflow.log_metric("r2", r2)
+        mlflow.log_metric("mae", mae)
+        mlflow.log_artifact(DATA_PATH)
+        print("Save to: {}".format(mlflow.get_artifact_uri()))
+        tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
+
+        mlflow.sklearn.log_model(lr, "model")
+
+
+train(0.5, 0.5)
